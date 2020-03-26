9e2e7167498c31b05bb8424092d77bd9ddee556a
Michael Skeen <mmskeen@gmail.com>
2020-03-25 18:05:27 -0600

    Broke up into components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "./contacts";
+import Avatar from "./Avatar";
 
 function App() {
   const cards = contacts.map(c => (
@@ -8,6 +9,7 @@ function App() {
   ));
   return (
     <div>
+      <Avatar img="https://mmskeen.github.io/images/profile.jpeg" />
       <h1 className="heading">My Contacts</h1>
       {cards}
     </div>
